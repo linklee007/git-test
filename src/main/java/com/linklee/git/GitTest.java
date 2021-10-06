4839bdd1072db6cdaf0711de73d2f792ffe33b5c
link <lyf0925@qq.com>
2021-10-06 11:01:26 +0800

    first commit

---
+++ src/main/java/com/linklee/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.linklee.git;
+
+/**
+ * @author linklee
+ * @create 2021-10-06 10:54
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello world!!!");
+    }
+}
